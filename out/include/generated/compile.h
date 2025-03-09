d6431a30c26e9ad79c9571080d92676631fbb36e
UdyneOS <51253393+nurfinzmuhamad14@users.noreply.github.com>
2025-03-09 05:01:56 +0000

    TEST

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Tue Feb 18 17:31:27 UTC 2025"
-#define LINUX_COMPILE_BY "UdyneOS"
-#define LINUX_COMPILE_HOST "Droidx"
-#define LINUX_COMPILER "Android (6443078 based on r383902) clang version 11.0.1 (https://android.googlesource.com/toolchain/llvm-project b397f81060ce6d701042b782172ed13bee898b79), GNU ld (GNU Binutils for Ubuntu) 2.38"
