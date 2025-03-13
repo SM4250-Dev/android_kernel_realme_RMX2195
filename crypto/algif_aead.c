b6b0f6ec9d8deb71dcf5527eb38325071755567c
ShivamKumarJha <jha.shivam3@gmail.com>
2025-03-13 06:02:44 +0000

    Add crypto/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- crypto/algif_aead.c
+++ crypto/algif_aead.c
@@ -81,8 +81,16 @@ static int crypto_aead_copy_sgl(struct crypto_sync_skcipher *null_tfm,
 {
 	SYNC_SKCIPHER_REQUEST_ON_STACK(skreq, null_tfm);
 
+/*<<<<<<< HEAD
 	skcipher_request_set_sync_tfm(skreq, null_tfm);
+	skcipher_request_set_callback(skreq, CRYPTO_TFM_REQ_MAY_BACKLOG,
+||||||| ad326970d25c
+	skcipher_request_set_tfm(skreq, null_tfm);
+	skcipher_request_set_callback(skreq, CRYPTO_TFM_REQ_MAY_BACKLOG,
+=======*/
+	skcipher_request_set_tfm(skreq, null_tfm);
 	skcipher_request_set_callback(skreq, CRYPTO_TFM_REQ_MAY_SLEEP,
+/*>>>>>>> 877d4a14eea6cdf3a4579579c72a1f1eeaf8878f*/
 				      NULL, NULL);
 	skcipher_request_set_crypt(skreq, src, dst, len, NULL);
 
@@ -296,11 +304,13 @@ static int _aead_recvmsg(struct socket *sock, struct msghdr *msg,
 
 		aead_request_set_callback(&areq->cra_u.aead_req,
 					  CRYPTO_TFM_REQ_MAY_SLEEP,
+					  CRYPTO_TFM_REQ_MAY_SLEEP,
 					  af_alg_async_cb, areq);
 		err = ctx->enc ? crypto_aead_encrypt(&areq->cra_u.aead_req) :
 				 crypto_aead_decrypt(&areq->cra_u.aead_req);
 
 		/* AIO operation in progress */
+		if (err == -EINPROGRESS)
 		if (err == -EINPROGRESS)
 			return -EIOCBQUEUED;
 
@@ -308,6 +318,7 @@ static int _aead_recvmsg(struct socket *sock, struct msghdr *msg,
 	} else {
 		/* Synchronous operation */
 		aead_request_set_callback(&areq->cra_u.aead_req,
+					  CRYPTO_TFM_REQ_MAY_SLEEP |
 					  CRYPTO_TFM_REQ_MAY_SLEEP |
 					  CRYPTO_TFM_REQ_MAY_BACKLOG,
 					  crypto_req_done, &ctx->wait);
