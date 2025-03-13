469d79a397c91190c4f8b16a1a8674a2c3836cfe
ShivamKumarJha <jha.shivam3@gmail.com>
2025-03-13 06:02:50 +0000

    Add include/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- include/linux/fscrypt.h
+++ include/linux/fscrypt.h
@@ -36,6 +36,7 @@ struct fscrypt_name {
 	u32 hash;
 	u32 minor_hash;
 	struct fscrypt_str crypto_buf;
+	//bool is_ciphertext_name;
 	bool is_ciphertext_name;
 };
 
@@ -675,6 +676,7 @@ static inline int fscrypt_require_key(struct inode *inode)
  *
  * Return: 0 on success, -ENOKEY if the directory's encryption key is missing,
  * -EXDEV if the link would result in an inconsistent encryption policy, or
+ * -EXDEV if the link would result in an inconsistent encryption policy, or
  * another -errno code.
  */
 static inline int fscrypt_prepare_link(struct dentry *old_dentry,
@@ -682,6 +684,7 @@ static inline int fscrypt_prepare_link(struct dentry *old_dentry,
 				       struct dentry *dentry)
 {
 	if (IS_ENCRYPTED(dir))
+		return __fscrypt_prepare_link(d_inode(old_dentry), dir, dentry);
 		return __fscrypt_prepare_link(d_inode(old_dentry), dir, dentry);
 	return 0;
 }
@@ -706,6 +709,7 @@ static inline int fscrypt_prepare_link(struct dentry *old_dentry,
  * in an encrypted directory tree use the same encryption policy.
  *
  * Return: 0 on success, -ENOKEY if an encryption key is missing, -EXDEV if the
+ * Return: 0 on success, -ENOKEY if an encryption key is missing, -EXDEV if the
  * rename would cause inconsistent encryption policies, or another -errno code.
  */
 static inline int fscrypt_prepare_rename(struct inode *old_dir,
@@ -726,7 +730,11 @@ static inline int fscrypt_prepare_rename(struct inode *old_dir,
  * @dir: directory being searched
  * @dentry: filename being looked up
  * @fname: (output) the name to use to search the on-disk directory
+ * @fname: (output) the name to use to search the on-disk directory
  *
+ * Prepare for ->lookup() in a directory which may be encrypted by determining
+ * the name that will actually be used to search the directory on-disk.  Lookups
+ * can be done with or without the directory's encryption key; without the key,
  * Prepare for ->lookup() in a directory which may be encrypted by determining
  * the name that will actually be used to search the directory on-disk.  Lookups
  * can be done with or without the directory's encryption key; without the key,
@@ -737,6 +745,9 @@ static inline int fscrypt_prepare_rename(struct inode *old_dir,
  * operations contain fscrypt_d_revalidate if DCACHE_ENCRYPTED_NAME was set,
  * so that the dentry can be invalidated if the key is later added.
  *
+ * Return: 0 on success; -ENOENT if key is unavailable but the filename isn't a
+ * correctly formed encoded ciphertext name, so a negative dentry should be
+ * created; or another -errno code.
  * Return: 0 on success; -ENOENT if key is unavailable but the filename isn't a
  * correctly formed encoded ciphertext name, so a negative dentry should be
  * created; or another -errno code.
@@ -752,7 +763,7 @@ static inline int fscrypt_prepare_lookup(struct inode *dir,
 	fname->usr_fname = &dentry->d_name;
 	fname->disk_name.name = (unsigned char *)dentry->d_name.name;
 	fname->disk_name.len = dentry->d_name.len;
-	return 0;
+        return 0;
 }
 
 /**
